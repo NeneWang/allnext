72bd93c290b40a908f00069bac6c92f2a5ac2fe1
NelsonWang <wangnelson2@gmail.com>
2021-08-06 07:04:10 -0400

    bootstrap and custom  css

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,11 @@ import { Provider } from "next-auth/client";
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 
+// add bootstrap css 
+import 'bootstrap/dist/css/bootstrap.css'
+// own css files here
+import "../css/customcss.css";
+
 function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
